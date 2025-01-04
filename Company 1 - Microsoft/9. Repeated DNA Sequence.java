345fd00371468babe1f5f2f0f8fe6c76c5e44801
Drishti Rai <114906473+DrishtiRai777@users.noreply.github.com>
2025-01-04 22:28:04 +0530

    Create 9. Repeated DNA Sequence.java

---
+++ Company 1 - Microsoft/9. Repeated DNA Sequence.java
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/repeated-dna-sequences/
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int l = 0, r = 10;
+        List<String> list = new ArrayList<>();
+        while(r <= s.length()) {
+            String substring = s.substring(l, r);
+            map.put(substring, map.getOrDefault(substring, 0) + 1);
+            l++;
+            r++;
+        }
+
+        for(String dna : map.keySet()) {
+            int val = map.get(dna);
+            if (val > 1) {
+                list.add(dna);
+            }
+        }
+
+        return list;
+    }
+}
