f7d8d97ad21bc7b7e66aa89646e5ac6c56eace1f
Drishti Rai <114906473+DrishtiRai777@users.noreply.github.com>
2025-01-21 23:01:02 +0530

    Create 9. Query Kth Smallest Trimmed Number.java

---
+++ Company 4 - Google/9. Query Kth Smallest Trimmed Number.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/query-kth-smallest-trimmed-number/description/
+class Solution {
+    public int helper(String[] nums, int k, int trim) {
+        List<Pair<String, Integer>> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            String num = nums[i];
+            String trimmed = num.substring(num.length() - trim); 
+            list.add(new Pair(trimmed, i));
+        }
+
+        Collections.sort(list, (a, b) -> a.getKey().compareTo(b.getKey()));
+        return list.get(k-1).getValue(); 
+    }
+
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] q) {
+        int[] res = new int[q.length];
+        int idx = 0;
+        
+        for(int i = 0; i < q.length; i++) {
+            int k = q[i][0];
+            int trim = q[i][1];
+            res[idx++] = helper(nums, k, trim); 
+        }
+
+        return res;
+    }
+}
